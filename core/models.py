2905890ee6bd9839b748acbec07eff4ef734d78a
Allan Imire <imireallan@gmail.com>
2018-07-16 15:33:29 +0300

    added the entries model

---
+++ core/models.py
@@ -0,0 +1,16 @@
+"""Contains data structures that store data in memory."""
+
+from datetime import datetime
+
+class Entry(object):
+    """Data structure for the entries."""
+    def __init__(self):
+        self.entries = []
+
+    def create_entry(self, data):
+        data['id'] = int(len(self.entries) + 1)
+        data['title'] = data['title'].strip()
+        data['contents'] = data['contents'].strip()
+        data['date_created'] = datetime.utcnow()
+        data['date_modified'] = datetime.utcnow()
+        self.entries.append(data)
