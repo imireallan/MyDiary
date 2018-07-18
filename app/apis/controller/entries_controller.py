5baba894aea13f6c5479d0b1d2fba95f1ace7943
Allan Imire <imireallan@gmail.com>
2018-07-18 09:35:42 +0300

    [Feature #159065510] created post endpoint

--- app/apis/controller/entries_controller.py
+++ app/apis/controller/entries_controller.py
@@ -9,5 +9,16 @@ from ..utils.dto import EntriesDto
 api = EntriesDto.api
 entries = EntriesDto.entries
 
-
 entry = EntryClass()
+
+@api.route("/entries")
+class EntryList(Resource):
+    """Displays a list of all entries and lets you POST to add new entries."""
+
+    @api.expect(entries)
+    @api.doc('creates an entry')
+    @api.response(201, "Created")
+    @api.marshal_with(entries)
+    def post(self):
+        """Creates a new Entry."""
+        return entry.create_entry(api.payload),201
