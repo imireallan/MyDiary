eee9ae14274582e3378c360f8a594e0bc31cf6ec
Allan Imire <imireallan@gmail.com>
2018-07-24 23:26:49 +0300

    [chore

---
+++ database.py
@@ -0,0 +1,48 @@
+import psycopg2
+import psycopg2.extras
+
+class DataBaseConnection:
+    def __init__(self):
+        connect_db(self)
+
+        
+    def connect_db(self):
+        """Method for creating db connection."""
+        try:
+            self.connection = psycopg2.connect(database="diary_api", user="imire", password="pass@word1", host="localhost", port="5432")
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+            self.dict_cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        except Exception as e:
+            return {"message": str(e)}
+
+    def create_tables(self):
+        tables=(
+            """
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE entries (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                contents text,
+                FOREIGN KEY (user_id)
+                    REFERENCES users (id)
+                    ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            """
+        )
+        for table in tables:
+            self.cursor.execute(table)
+        
+
+if __name__=="__main__":
+    db_connection = DataBaseConnection()
+    db_connection.create_tables()
